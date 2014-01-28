43c8322185faca3ee9a781e8038f4bcc4536d684
chrhsmt <chr@chrhsmt.com>
2014-01-28 19:41:50 +0900

    correspond to categories

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -8,12 +8,20 @@ class DataController < ApplicationController
   # before_action :set_datum, only: [:show, :edit, :update, :destroy]
 
   def index
+  	categoriesJson = JSON.parser.new(open(Settings.shirasete_api.issue_categories).read).parse
+  	@categories = categoriesJson['issue_categories'].unshift({"name" => "全体", "id" => "all" })
   end
 
   def download
+  	category = params[:category]
+  	urlParam = ''
+  	if category.present? && category != 'all'
+  		urlParam = "&category_id=#{category}"
+  	end
 
   	# call api
-  	url = Settings.shirasete_api.open_issues
+  	url = Settings.shirasete_api.open_issues + urlParam
+  	logger.debug url
 	json = JSON.parser.new(open(url).read)
 
   	# generate csv data
