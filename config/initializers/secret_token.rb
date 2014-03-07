1f59d8f66719a2cbad7d84758460e31d54735c05
chrhsmt <chr@chrhsmt.com>
2014-03-07 23:20:21 +0900

    secret token from env

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-IeiriPoster::Application.config.secret_key_base = '4da8cf1f4d6466a8fc65b6b6f4947ae0468877c270f5ce6acc2644813bc82a86e5db4147d7e0cc045c3569bc80a83bdf1d4f1183ef4c3d6a1ee6018a206ac3b7'
+IeiriPoster::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || '4da8cf1f4d6466a8fc65b6b6f4947ae0468877c270f5ce6acc2644813bc82a86e5db4147d7e0cc045c3569bc80a83bdf1d4f1183ef4c3d6a1ee6018a206ac3b7'
